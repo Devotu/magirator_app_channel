515c28ddd8552cbc04eb142af181fd2042ab5196
DevotuOne <devotu.developer@gmail.com>
2018-12-08 23:02:05 +0100

    Game - Confirm, Comment

--- lib/magirator_app_channel/stores/game_store.ex
+++ lib/magirator_app_channel/stores/game_store.ex
@@ -1,162 +1,227 @@
 defmodule MagiratorAppChannel.GameStore do
     
-    alias Bolt.Sips, as: Bolt
-    import Ecto.Changeset
-    require Logger
+  alias Bolt.Sips, as: Bolt
+  import Ecto.Changeset
+  require Logger
 
-    alias MagiratorAppChannel.Player
-    alias MagiratorAppChannel.Deck
-    alias MagiratorAppChannel.Result
-    alias MagiratorAppChannel.Game
-    alias MagiratorAppChannel.GameResultSet
-    import MagiratorAppChannel.IdStore
+  alias MagiratorAppChannel.Player
+  alias MagiratorAppChannel.Deck
+  alias MagiratorAppChannel.Result
+  alias MagiratorAppChannel.Game
+  alias MagiratorAppChannel.GameResultSet
+  import MagiratorAppChannel.IdStore
 
-    alias MagiratorAppChannel.Streamliner
+  alias MagiratorAppChannel.Streamliner
 
-    @doc """
-    Creates a game node ready to tie the results together
-    A full registerd game contains several nodes of which the actual game node is the hub
+  @doc """
+  Creates a game node ready to tie the results together
+  A full registerd game contains several nodes of which the actual game node is the hub
+  """
+  def create( game ) do
+    
+    { :ok, generated_id } = next_id()
+
+    query = """
+    MATCH 
+      (u:User)-[:Is]->(p:Player) 
+      WHERE 
+      u.id = #{ game.creator } 
+      CREATE 
+      (p)-[:Created]->(g:Game { id:#{ generated_id }, created:TIMESTAMP(), conclusion: "#{ game.conclusion }" })
+      RETURN g.id as id;
     """
-    def create( game ) do
-        
-        { :ok, generated_id } = next_id()
+    
+    result = Bolt.query!(Bolt.conn, query)
+    [ row ] = result
+    { created_id } = { row["id"] }
+
+    case created_id == generated_id do
+      :true ->
+        { :ok, created_id }
+      :false ->
+        { :error, :insert_failure }
+    end
+  end
+
+  @doc """
+  Connects a single result to a given game node
+  """
+  def add_result( game_id, deck_id, result ) do
+    
+    { :ok, generated_id } = next_id()
+
+    query = """
+    MATCH 
+      (d:Deck), 
+      (g:Game) 
+      WHERE 
+      d.id = #{ deck_id } 
+      AND g.id = #{ game_id } 
+      CREATE 
+      (d)-[:Got]->
+      (r:Result { 
+        id: #{ generated_id }, 
+        created: TIMESTAMP(), 
+        place: #{ result.place }, 
+        comment: '#{ result.comment }',
+        confirmed: false
+        })
+      -[:In]->(g)
+      RETURN r.id as id;
+    """
+    
+    result = Bolt.query!(Bolt.conn, query)
+    [ row ] = result
+    { created_id } = { row["id"] }
+
+    case created_id == generated_id do
+      :true ->
+        { :ok, created_id }
+      :false ->
+        { :error, :insert_failure }
+    end
+  end
+
+  def confirm_result( result ) do
+
+    result_map = Streamliner.changeset_struct_to_map result
+
+    case result_map.id do
+      nil ->
+        {:error, :no_id}
+      _ ->
 
         query = """
         MATCH 
-          (u:User)-[:Is]->(p:Player) 
-         WHERE 
-          u.id = #{ game.creator } 
-         CREATE 
-          (p)-[:Created]->(g:Game { id:#{ generated_id }, created:TIMESTAMP(), conclusion: "#{ game.conclusion }" })
-         RETURN g.id as id;
+          (r:Result)
+          WHERE 
+            r.id = #{ result_map.id } 
+          SET
+            r.confirmed = true
+          RETURN r.id as id;
         """
         
         result = Bolt.query!(Bolt.conn, query)
         [ row ] = result
-        { created_id } = { row["id"] }
+        { updated_id } = { row["id"] }
 
-        case created_id == generated_id do
-            :true ->
-                { :ok, created_id }
-            :false ->
-                { :error, :insert_failure }
+        case updated_id == result_map.id do
+          :true ->
+            { :ok, updated_id }
+          :false ->
+            { :error, :update_failure }
         end
     end
+  end
 
-    @doc """
-    Connects a single result to a given game node
-    """
-    def add_result( game_id, deck_id, result ) do
-        
-        { :ok, generated_id } = next_id()
+  def comment_result( result ) do
+
+    result_map = Streamliner.changeset_struct_to_map result
+
+    case result_map.id do
+      nil ->
+        {:error, :no_id}
+      _ ->
 
         query = """
         MATCH 
-          (d:Deck), 
-          (g:Game) 
-         WHERE 
-          d.id = #{ deck_id } 
-          AND g.id = #{ game_id } 
-         CREATE 
-          (d)-[:Got]->
-          (r:Result { 
-              id: #{ generated_id }, 
-              created: TIMESTAMP(), 
-              place: #{ result.place }, 
-              comment: '#{ result.comment }' 
-            })
-          -[:In]->(g)
-         RETURN r.id as id;
+          (r:Result)
+          WHERE 
+            r.id = #{ result_map.id } 
+          SET
+            r.comment = '#{ result.comment }' 
+          RETURN r.id as id;
         """
         
         result = Bolt.query!(Bolt.conn, query)
         [ row ] = result
-        { created_id } = { row["id"] }
+        { updated_id } = { row["id"] }
 
-        case created_id == generated_id do
-            :true ->
-                { :ok, created_id }
-            :false ->
-                { :error, :insert_failure }
+        case updated_id == result_map.id do
+          :true ->
+            { :ok, updated_id }
+          :false ->
+            { :error, :update_failure }
         end
     end
+  end
 
-    def select_all_by_deck( deck_id ) do
+  def select_all_by_deck( deck_id ) do
 
-        Logger.debug Kernel.inspect deck_id
+    Logger.debug Kernel.inspect deck_id
 
-        query = """
-        MATCH 
-            (d:Deck)
-            -[:Got]->
-            (:Result)
-            -[:In]->
-            (g:Game) 
-        WHERE 
-            d.id = #{ deck_id } 
-        WITH 
-            g 
-        MATCH 
-            (g)
-            <-[:In]-
-            (r:Result)
-            <-[:Got]-
-            (d:Deck)
-            <-[:Possess]-
-            (p:Player), 
-            (p)
-            -[:Currently]->
-            (pd:Data),
-            (d)
-            -[:Currently]->
-            (dd:Data)  
-        RETURN 
-            g,d,p,r,pd,dd
-        """
+    query = """
+    MATCH 
+      (d:Deck)
+      -[:Got]->
+      (:Result)
+      -[:In]->
+      (g:Game) 
+    WHERE 
+      d.id = #{ deck_id } 
+    WITH 
+      g 
+    MATCH 
+      (g)
+      <-[:In]-
+      (r:Result)
+      <-[:Got]-
+      (d:Deck)
+      <-[:Possess]-
+      (p:Player), 
+      (p)
+      -[:Currently]->
+      (pd:Data),
+      (d)
+      -[:Currently]->
+      (dd:Data)  
+    RETURN 
+      g,d,p,r,pd,dd
+    """
 
-        Logger.debug "query:#{query}"
-        
-        result = Bolt.query!(Bolt.conn, query)
-        gamesets = nodes_to_game_result_sets result
+    Logger.debug "query:#{query}"
+    
+    result = Bolt.query!(Bolt.conn, query)
+    gamesets = nodes_to_game_result_sets result
 
-        { :ok, gamesets }
+    { :ok, gamesets }
+  end
+
+
+  defp nodes_to_game_result_sets( nodes ) do
+      Enum.map( nodes, &node_to_game_result_set/1 )
+  end
+
+  defp node_to_game_result_set( node ) do
+
+    player_data = Map.merge( node["p"].properties, node["pd"].properties )
+    player_changeset = Player.changeset( %Player{}, player_data )
+
+    deck_data = Map.merge( node["d"].properties, node["dd"].properties )
+    deck_changeset = Deck.changeset( %Deck{}, deck_data )
+
+    result_data = node["r"].properties
+    result_changeset = Result.changeset( %Result{}, result_data )
+
+    game_data =node["g"].properties
+    game_changeset = Game.changeset( %Game{}, game_data )
+
+    if player_changeset.valid? and deck_changeset.valid? and result_changeset.valid? and game_changeset.valid?  do
+
+      player_map = apply_to_map player_changeset
+      deck_map = apply_to_map deck_changeset
+      result_map = apply_to_map result_changeset
+      game_map = apply_to_map game_changeset
+
+      %GameResultSet{ player: player_map, deck: deck_map, result: result_map, game: game_map }
+    else
+      { :error, :invalid_data }
     end
+  end
 
-
-    defp nodes_to_game_result_sets( nodes ) do
-        Enum.map( nodes, &node_to_game_result_set/1 )
-    end
-
-    defp node_to_game_result_set( node ) do
-
-        player_data = Map.merge( node["p"].properties, node["pd"].properties )
-        player_changeset = Player.changeset( %Player{}, player_data )
-
-        deck_data = Map.merge( node["d"].properties, node["dd"].properties )
-        deck_changeset = Deck.changeset( %Deck{}, deck_data )
-
-        result_data = node["r"].properties
-        result_changeset = Result.changeset( %Result{}, result_data )
-
-        game_data =node["g"].properties
-        game_changeset = Game.changeset( %Game{}, game_data )
-
-        if player_changeset.valid? and deck_changeset.valid? and result_changeset.valid? and game_changeset.valid?  do
-
-            player_map = apply_to_map player_changeset
-            deck_map = apply_to_map deck_changeset
-            result_map = apply_to_map result_changeset
-            game_map = apply_to_map game_changeset
-
-            %GameResultSet{ player: player_map, deck: deck_map, result: result_map, game: game_map }
-        else
-            { :error, :invalid_data }
-        end
-    end
-
-    defp apply_to_map( changeset ) do
-        changeset
-        |> apply_changes 
-        |> Streamliner.changeset_struct_to_map
-    end
+  defp apply_to_map( changeset ) do
+    changeset
+    |> apply_changes 
+    |> Streamliner.changeset_struct_to_map
+  end
 end
--- test/application/stores/game_store_test.exs
+++ test/application/stores/game_store_test.exs
@@ -1,64 +1,129 @@
 defmodule GameStoreTest do
-    use ExUnit.Case
+  use ExUnit.Case
 
-    import MagiratorAppChannel.GameStore
+  import MagiratorAppChannel.GameStore
 
-    import Ecto.Changeset
-    alias MagiratorAppChannel.Game
-    alias MagiratorAppChannel.Result
-    alias MagiratorAppChannel.GameResultSet
+  import Ecto.Changeset
+  alias MagiratorAppChannel.Game
+  alias MagiratorAppChannel.Result
+  alias MagiratorAppChannel.GameResultSet
 
-    #Insert
-    test "create game" do
-        game_changeset = Game.changeset( %Game{}, %{ 
-            "conclusion" => "VICTORY", 
-            "created" => System.system_time(:seconds), 
-            "creator" => 1
-        })
-        { status, id } = create( apply_changes( game_changeset ) )
-        assert :ok == status
-        assert is_number id
-    end
-    
-    test "add results" do
-        game_changeset = Game.changeset( %Game{}, %{ 
-            "conclusion" => "VICTORY", 
-            "created" => System.system_time(:seconds), 
-            "creator" => 2
-        })
-        { status, id } = create( apply_changes( game_changeset ) )
+  #Insert
+  test "create game" do
+    game_changeset = Game.changeset( %Game{}, %{ 
+      "conclusion" => "VICTORY", 
+      "created" => System.system_time(:seconds), 
+      "creator" => 1
+    })
+    { status, id } = create( apply_changes( game_changeset ) )
+    assert :ok == status
+    assert is_number id
+  end
+  
+  test "add results" do
+    game_changeset = Game.changeset( %Game{}, %{ 
+      "conclusion" => "VICTORY", 
+      "created" => System.system_time(:seconds), 
+      "creator" => 2
+    })
+    { _, id } = create( apply_changes( game_changeset ) )
 
-        result_one_changeset = Result.changeset( %Result{}, %{ 
-            "place" => 1, 
-            "comment" => "Created by GameStoreTest - add result - 1", 
-            "created" => System.system_time(:seconds)
-        })
-        result_one = apply_changes( result_one_changeset )
-        { status_one, _ } = add_result( id, 22, result_one )
-        assert :ok == status_one
+    result_one_changeset = Result.changeset( %Result{}, %{ 
+      "place" => 1, 
+      "created" => System.system_time(:seconds)
+    })
+    result_one = apply_changes( result_one_changeset )
+    { status_one, _ } = add_result( id, 22, result_one )
+    assert :ok == status_one
 
-        result_two_changeset = Result.changeset( %Result{}, %{ 
-            "place" => 2, 
-            "comment" => "Created by GameStoreTest - add result - 2", 
-            "created" => System.system_time(:seconds)
-        })
-        result_two = apply_changes( result_two_changeset )
-        { status_two, _ } = add_result( id, 23, result_two )
-        assert :ok == status_two
-    end
+    result_two_changeset = Result.changeset( %Result{}, %{ 
+      "place" => 2, 
+      "created" => System.system_time(:seconds)
+    })
+    result_two = apply_changes( result_two_changeset )
+    { status_two, _ } = add_result( id, 23, result_two )
+    assert :ok == status_two
+  end
+
+  
+  test "confirm result" do
+    game_changeset = Game.changeset( %Game{}, %{ 
+      "conclusion" => "VICTORY", 
+      "created" => System.system_time(:seconds), 
+      "creator" => 3
+    })
+    { _, id } = create( apply_changes( game_changeset ) )
+
+    result_changeset = Result.changeset( %Result{}, %{ 
+      "place" => 1, 
+      "created" => System.system_time(:seconds)
+    })
+    result = apply_changes( result_changeset )
+    { status_add, id } = add_result( id, 23, result )
+    assert :ok == status_add
+
+    result_with_id_changeset = Result.changeset( %Result{}, %{ 
+      "id" => id,
+      "place" => result.place, 
+      "comment" => "GameStoreTest - confirm result"
+    })
+    result_with_id = apply_changes( result_with_id_changeset )
+    { status_confirm, _ } = confirm_result( result_with_id )
+    assert :ok == status_confirm
+  end
+
+  test "fail confirm result - no id" do
+    result_commented_changeset = Result.changeset( %Result{}, %{ 
+      "place" => 1, 
+      "comment" => "GameStoreTest - confirm result",
+      "created" => System.system_time(:seconds)
+    })
+    result_commented = apply_changes( result_commented_changeset )
+    { status, cause } = confirm_result( result_commented )
+    assert :error == status
+    assert :no_id == cause
+  end
+
+  
+  test "comment result" do
+    game_changeset = Game.changeset( %Game{}, %{ 
+      "conclusion" => "VICTORY", 
+      "created" => System.system_time(:seconds), 
+      "creator" => 3
+    })
+    { _, id } = create( apply_changes( game_changeset ) )
+
+    result_changeset = Result.changeset( %Result{}, %{ 
+      "place" => 1, 
+      "created" => System.system_time(:seconds),
+      "comment" => "This should not be the comment. GameStoreTest - comment result"
+    })
+    result = apply_changes( result_changeset )
+    { status_add, id } = add_result( id, 23, result )
+    assert :ok == status_add
+
+    result_with_id_changeset = Result.changeset( %Result{}, %{ 
+      "id" => id,
+      "place" => result.place, 
+      "comment" => "GameStoreTest - comment result"
+    })
+    result_with_id = apply_changes( result_with_id_changeset )
+    { status_comment, _ } = comment_result( result_with_id )
+    assert :ok == status_comment
+  end
 
 
-    #Select
-    test "select all games deck" do
-        { status, data } = select_all_by_deck 20
-        assert :ok == status
-        assert is_list data
-        assert %GameResultSet{} = List.first data
-    end
+  #Select
+  test "select all games deck" do
+    { status, data } = select_all_by_deck 20
+    assert :ok == status
+    assert is_list data
+    assert %GameResultSet{} = List.first data
+  end
 
-    test "select all games from deck with no games" do
-        { status, msg } = select_all_by_deck 99
-        assert :ok == status
-        assert [] == msg
-    end
+  test "select all games from deck with no games" do
+    { status, msg } = select_all_by_deck 99
+    assert :ok == status
+    assert [] == msg
+  end
 end
