6fd8e39534919acc78d0a6d6bc0ff9d21fc614d6
Unknown <devotu.developer@gmail.com>
2018-12-30 16:01:24 +0100

    Refactor - Remove unused

--- lib/magirator_app_channel_web/channels/main_channel.ex
+++ lib/magirator_app_channel_web/channels/main_channel.ex
@@ -10,53 +10,15 @@ defmodule MagiratorAppChannelWeb.MainChannel do
     def join("app:main", _message, socket) do
         Logger.debug "join app:main :ok"
         Logger.debug Kernel.inspect socket.assigns[:user_id]
-        {:ok, %{id: socket.assigns[:user_id]}, socket}
+
+        case is_number socket.assigns[:user_id] do
+            :true ->
+                {:ok, %{id: socket.assigns[:user_id]}, socket}
+            _ ->       
+                {:error, %{reason: "unauthorized"}}
+        end
     end
 
-    defp join( _, nil ) do        
-        {:error, %{reason: "unauthorized"}}
-    end
-
-    defp join( _, _ ) do        
-        {:error, %{reason: "unknown error"}}
-    end
-
-    
-    def handle_in("new_msg", %{"msg" => msg}, socket) do
-        user_id = socket.assigns[:user_id]
-
-        # query = """
-        # MATCH (n) WHERE id(n) = 164 RETURN n.name as name, n.created as id
-        # """
-
-        # now = now()
-
-        # Logger.debug "now:#{now}"
-        # Logger.debug "query:#{query}"
-
-        
-        # result = Bolt.query!(Bolt.conn, query)
-        # Logger.debug( Kernel.inspect( result ) )
-        # [foo] = result
-        # Logger.debug( Kernel.inspect( foo ) )
-        # {bar, baz} = {foo["name"], foo["id"]}
-        # Logger.debug( Kernel.inspect( bar ) )
-        # Logger.debug( Kernel.inspect( baz ) )
-    
-        # broadcast(socket, "new_msg", %{msg: msg, user: user, data: bar})
-        broadcast(socket, "new_msg", %{msg: msg, user_id: user_id, data: now()})
-        {:reply, :ok, socket}
-    end
-
-
-    def handle_in("newdeck", %{"name" => name, "theme" => theme}, socket) do
-        user_id = socket.assigns[:user_id]
-
-        response = "Got deck #{name} with theme #{theme}"
-
-        broadcast(socket, "new_msg", %{msg: response, user_id: user_id, data: now()})
-        {:reply, :ok, socket}
-    end
 
     @doc """
     String domain:action, mr.data.struct data, socket socket
@@ -99,9 +61,5 @@ defmodule MagiratorAppChannelWeb.MainChannel do
                 {:reply, {:error, %{cause: msg}} , socket}
         end
 
-    end
-
-    defp now do
-        System.system_time(:seconds)
     end
 end
